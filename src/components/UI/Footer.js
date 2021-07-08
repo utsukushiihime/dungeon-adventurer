e95c9dc3519302f4743298fd52b8e1b725d10354
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-07 22:55:05 -0500

    bumps packages

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { TiHeart } from "react-icons/ti";
 
 const Footer = () => {
     return (
         <>
-            <footer class="footer mt-auto py-3 fixed-bottom bg-dark">
-                <div class="container">
-                    <span class="text-muted">
-                        Place sticky footer content here.
-                    </span>
+            <footer className="footer mt-auto py-3 fixed-bottom bg-dark">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-8">
+                            <span className="text-muted">
+                                &copy; {new Date().getFullYear()} - Dungeon
+                                Adventurer. All Rights Reserved. Founded in
+                                2021.
+                            </span>
+                        </div>
+                        <div className="col-4 text-right">
+                            <span className="text-muted float-end text-right">
+                                Made with <TiHeart className="text-danger" /> by
+                                Creativarian.
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </>
